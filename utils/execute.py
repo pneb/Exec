1438886b68fa18926f3270108d9cba7e677e0bf1
Bernward Sanchez <sanzbernward@gmail.com>
2022-12-30 10:45:19 +0800

    Update execute.py

--- utils/execute.py
+++ utils/execute.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 def execute(program_file, python_code):
+    print("Executing: {} {}".format(program_file, python_code))
     if sys.version_info < (3,0):
         os.system("python -c '{}' {}".format(python_code, program_file))
     else:
